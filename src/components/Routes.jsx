d413eba587521c5c354f9b578e220450440731b4
malan88 <m@stdwtr.io>
2020-10-27 15:16:06 -0400

    Add textcomponent

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,40 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Route, Switch} from 'react-router-dom'
 import routes from '../constants/routes'
 import {useAppState} from "../providers/AppStateProvider";
+import axios from 'axios';
 
-const FillerComponent = () => {
-  return (
-    <h1>HI</h1>
-  )
+const url = 'http://192.168.0.202:5000/_api';
+
+const TextsComponent = () => {
+    const [texts, setTexts] = useState([]);
+
+    useEffect(() => {
+        axios.get(url + '/text/all')
+            .then(resp => {
+                setTexts(resp.data.results);
+            }, error => {
+                console.log(error);
+            });
+    }, [url]);
+
+    return (
+        <div>
+            <ul>
+                {texts.map((text, index) => (
+                    <li key={text.slug}>
+                        <h1>{text.name}</h1>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+
 }
-
-const FillerComponent2 = () => {
-  return (
-    <h1>HI2</h1>
-  )
-}
-
-const FillerComponent3 = () => {
-  const [{theme,toggleTheme}] = useAppState()
-  console.log(theme)
-  return (
-    <>
-      <h1>HI3</h1>
-      <button onClick={toggleTheme}>Change theme</button>
-    </>
-  )
-}
-
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path={routes.Home} component={FillerComponent} />
-      <Route exact path={routes.Users} component={FillerComponent2} />
-      <Route exact path={routes.Writers} component={FillerComponent3} />
-      <Route exact path={routes.Texts} component={FillerComponent2} />
-      <Route exact path={routes.Tags} component={FillerComponent} />
+      <Route exact path={routes.Home} component={TextsComponent} />
+      <Route exact path={routes.Users} component={TextsComponent} />
+      <Route exact path={routes.Writers} component={TextsComponent} />
+      <Route exact path={routes.Texts} component={TextsComponent} />
+      <Route exact path={routes.Tags} component={TextsComponent} />
     </Switch>
   )
 }
