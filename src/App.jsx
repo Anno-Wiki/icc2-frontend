d025662f9384a37f2c0e4f36c0ae19c38a28c8ea
malan88 <m@stdwtr.io>
2020-11-15 06:37:56 -0500

    Add css reset, fonts to import, and global theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,28 @@ import React from 'react';
 
 import {BrowserRouter as Router} from 'react-router-dom'
 
+import './global/reset.css';
 import './App.css';
-import Routes from "./components/Routes";
-import Layout from "./components/layout/Layout";
-import AppStateProvider from "./providers/AppStateProvider";
+
+import Routes from './components/Routes';
+import Layout from './components/layout/Layout';
+
+import AppStateProvider from './providers/AppStateProvider';
+
+import { ThemeProvider } from 'styled-components';
+import { theme } from './global/theme';
 
 function App() {
   return (
-    <Router>
-      <AppStateProvider>
-        <Layout>
-          <Routes />
-        </Layout>
-      </AppStateProvider>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AppStateProvider>
+          <Layout>
+            <Routes />
+          </Layout>
+        </AppStateProvider>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import baseRoutes from '../../constants/routes'
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+display: flex;
+justify-content: space-around;
+`
+const Link = styled(NavLink)`
+text-decoration: none;
+`
 
 const Header = () => {
   const routesAsText = Object.entries(baseRoutes)
   return (
-    <div>
-      {routesAsText.map(linkInfo => <NavLink to={linkInfo[1]}>{linkInfo[0]}</NavLink>)}
-    </div>
+    <Nav>
+      {routesAsText.map(linkInfo => <Link to={linkInfo[1]}>{linkInfo[0]}</Link>)}
+    </Nav>
   )
 }
 
