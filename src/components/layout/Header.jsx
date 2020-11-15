7a300fca90f00f061672b86ddf5eb2f39c03825e
malan88 <m@stdwtr.io>
2020-11-15 07:09:11 -0500

    Style header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,9 +6,20 @@ import styled from 'styled-components';
 const Nav = styled.nav`
 display: flex;
 justify-content: space-around;
+padding: 1rem 0;
+margin: 0 auto;
+width: 65%;
 `
 const Link = styled(NavLink)`
 text-decoration: none;
+font-family: ${props => props.theme.font.display};
+font-weight: bold;
+font-size: 3rem;
+color: ${props => props.theme.color.black};
+transition: color ${props => props.theme.transitionShort} linear;
+&:hover {
+  color: ${props => props.theme.color.gray};
+}
 `
 
 const Header = () => {
--- src/global/theme.js
+++ src/global/theme.js
@@ -1,3 +1,12 @@
 export const theme = {
-  temp: 'nothing'
+  font: {
+    display: 'Linux Biolinum',
+    text: 'EB Garamond'
+  },
+  color: {
+    black: '#212529',
+    gray: '#6c757d',
+    lightGray: '#ADB5BD',
+  },
+  transitionShort: '0.2s',
 }
