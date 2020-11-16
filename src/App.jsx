b8744741fea2c75adbb1d5b34367e100003a6c1f
ehickey08 <ehickey@academicanalytics.com>
2020-11-16 15:46:21 -0500

    add global styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import './global/reset.css';
-import './App.css';
 
 import Routes from './components/Routes';
 import Layout from './components/layout/Layout';
@@ -12,10 +11,12 @@ import AppStateProvider from './providers/AppStateProvider';
 
 import { ThemeProvider } from 'styled-components';
 import { theme } from './global/theme';
+import GlobalStyle from "./global/GlobalStyle";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Router>
         <AppStateProvider>
           <Layout>
