3d217af9dc5d7ca7eca4b926f8e52c3a0ec67bb8
malan88 <m@stdwtr.io>
2021-04-03 19:15:47 -0400

    Convert Editor to a function component

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import RichTextEditor from 'react-rte';
 import styled from 'styled-components';
 
@@ -65,77 +65,71 @@ background-color: ${({theme}) => theme.color.lightGray};
   color: white;
 }
 `
+const StatefulEditor = ({ updateState, editorState, selection }) => {
+  const [value, setValue] = useState(RichTextEditor.createEmptyValue());
 
-class StatefulEditor extends Component {
-  state = {
-    value: RichTextEditor.createEmptyValue()
-  };
-
-  onChange = (value) => {
-    this.setState({value});
-  };
-
-  onClick = () => {
-    console.log(this.state.value.toString('html'));
-    this.props.updateState(false);
-  };
-
-  render () {
-    const toolbarConfig = {
-      // Optionally specify the groups to display (displayed in the order listed).
-      display: ['INLINE_STYLE_BUTTONS', 'BLOCK_TYPE_BUTTONS', 'LINK_BUTTONS', 'BLOCK_TYPE_DROPDOWN', 'HISTORY_BUTTONS'],
-      INLINE_STYLE_BUTTONS: [
-        {label: 'Bold', style: 'BOLD', className: 'custom-css-class'},
-        {label: 'Italic', style: 'ITALIC'},
-        {label: 'Underline', style: 'UNDERLINE'},
-        {label: 'Code', style: 'CODE'},
-        {label: 'Strikethrough', style: 'STRIKETHROUGH'},
-      ],
-      BLOCK_TYPE_DROPDOWN: [
-        {label: 'Normal', style: 'unstyled'},
-        {label: 'Heading Large', style: 'header-one'},
-        {label: 'Heading Medium', style: 'header-two'},
-        {label: 'Heading Small', style: 'header-three'},
-        {label: 'Quote', style: 'blockquote'},
-      ],
-      BLOCK_TYPE_BUTTONS: [
-        {label: 'UL', style: 'unordered-list-item'},
-        {label: 'OL', style: 'ordered-list-item'}
-      ]
-    };
-    return (
-      <MasterParent editorState={this.props.editorState}>
-        <Parent>
-          <CloseBar>
-            <Close
-              style={{cursor: "pointer"}}
-              onClick={() => this.props.updateState(false)}
-            >&times;</Close>
-          </CloseBar>
-          <h3 style={{ marginLeft: "1rem" }}>Annotating:</h3>
-          <div
-            style={{ padding: "1rem" }}
-          >
-            {this.props.selection.selection}
-          </div>
-          <RichTextEditor
-            value={this.state.value}
-            onChange={this.onChange}
-            toolbarConfig={toolbarConfig}
-            className={EditorClassName}
-          />
-          <ButtonBar>
-            <Submit onClick={this.onClick}>Submit</Submit>
-            <Submit
-              onClick={() => this.props.updateState(false)}
-            >
-              Cancel
-            </Submit>
-          </ButtonBar>
-        </Parent>
-      </MasterParent>
-    );
+  const handleChange = (val) => {
+    setValue(val);
   }
-}
+
+  const click = () => {
+    // updateState is passed as a prop to hide the component
+    updateState(false);
+  }
+  const toolbarConfig = {
+    // Optionally specify the groups to display (displayed in the order listed).
+    display: ['INLINE_STYLE_BUTTONS', 'BLOCK_TYPE_BUTTONS', 'LINK_BUTTONS', 'BLOCK_TYPE_DROPDOWN', 'HISTORY_BUTTONS'],
+    INLINE_STYLE_BUTTONS: [
+      {label: 'Bold', style: 'BOLD', className: 'custom-css-class'},
+      {label: 'Italic', style: 'ITALIC'},
+      {label: 'Underline', style: 'UNDERLINE'},
+      {label: 'Code', style: 'CODE'},
+      {label: 'Strikethrough', style: 'STRIKETHROUGH'},
+    ],
+    BLOCK_TYPE_DROPDOWN: [
+      {label: 'Normal', style: 'unstyled'},
+      {label: 'Heading Large', style: 'header-one'},
+      {label: 'Heading Medium', style: 'header-two'},
+      {label: 'Heading Small', style: 'header-three'},
+      {label: 'Quote', style: 'blockquote'},
+    ],
+    BLOCK_TYPE_BUTTONS: [
+      {label: 'UL', style: 'unordered-list-item'},
+      {label: 'OL', style: 'ordered-list-item'}
+    ]
+  };
+  return (
+    <MasterParent editorState={editorState}>
+      <Parent>
+        <CloseBar>
+          <Close
+            style={{cursor: "pointer"}}
+            onClick={() => updateState(false)}
+          >&times;</Close>
+        </CloseBar>
+        <h3 style={{ marginLeft: "1rem" }}>Annotating:</h3>
+        <div
+          style={{ padding: "1rem" }}
+        >
+          {selection.selection}
+        </div>
+        <RichTextEditor
+          value={value}
+          onChange={handleChange}
+          toolbarConfig={toolbarConfig}
+          className={EditorClassName}
+        />
+        <ButtonBar>
+          <Submit onClick={click}>Submit</Submit>
+          <Submit
+            onClick={() => updateState(false)}
+          >
+            Cancel
+          </Submit>
+        </ButtonBar>
+      </Parent>
+    </MasterParent>
+  );
+};
 
 export default StatefulEditor
