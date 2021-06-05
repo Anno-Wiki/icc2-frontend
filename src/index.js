a9d120d51e13383b24a523bcc604ad56dc0de22b
malan88 <m@stdwtr.io>
2021-06-05 15:31:02 -0400

    Remove some auth0 provided stuff

--- src/index.js
+++ src/index.js
@@ -2,23 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
-
-const domain = process.env.REACT_APP_AUTH0DOMAIN;
-const audience = process.env.REACT_APP_AUTH0AUDIENCE;
-const clientId = process.env.REACT_APP_AUTH0CLIENTID;
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}
-    audience={audience}
-    scope="post:annotation post:edit"
-  >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Auth0Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
